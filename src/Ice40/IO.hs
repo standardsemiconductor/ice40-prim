0b12a6be4e4296b7b0686fb9513654f05d8ee083
dopamane <dwc1295@gmail.com>
2021-02-13 02:55:03 -0500

    add haddocks

--- src/Ice40/IO.hs
+++ src/Ice40/IO.hs
@@ -8,13 +8,7 @@ Maintainer  : standardsemiconductor@gmail.com
 IO hard IP primitive from Lattice Ice Technology Library https://github.com/standardsemiconductor/VELDT-info/blob/master/SBTICETechnologyLibrary201708.pdf
 -}
 module Ice40.IO where
-{-
-  ( io
-  , PinInput(..)
-  , PinOutput(..)
-  , IOStandard(..)
-  ) where
--}
+
 import Clash.Prelude
 import Clash.Annotations.Primitive
 import Data.String.Interpolate (i)
@@ -72,23 +66,24 @@ import Data.String.Interpolate.Util (unindent)
   ]
   |]) #-}
 
+-- | IO primitive, see io for wrapper
 {-# NOINLINE ioPrim #-}
 ioPrim
-  :: BitVector 6         -- ARG[0]  pinType
-  -> Bit                 -- ARG[1]  pullup
-  -> Bit                 -- ARG[2]  negTrigger
-  -> String              -- ARG[3]  ioStandard
-  -> Signal domIn Bit    -- ARG[4]  latchInputValue
-  -> Signal domEn Bit    -- ARG[5]  clockEnable
-  -> Clock domIn         -- ARG[6]  inputClk
-  -> Clock domOut        -- ARG[7]  outputClk
-  -> Signal domOut Bit   -- ARG[8]  outputEnable
-  -> Signal domOut Bit   -- ARG[9]  dOut0
-  -> Signal domOut Bit   -- ARG[10] dOut1
+  :: BitVector 6         -- ^ pinType
+  -> Bit                 -- ^ pullup
+  -> Bit                 -- ^ negTrigger
+  -> String              -- ^ ioStandard
+  -> Signal domIn Bit    -- ^ latchInputValue
+  -> Signal domEn Bit    -- ^ clockEnable
+  -> Clock domIn         -- ^ inputClk
+  -> Clock domOut        -- ^ outputClk
+  -> Signal domOut Bit   -- ^ outputEnable
+  -> Signal domOut Bit   -- ^ dOut0
+  -> Signal domOut Bit   -- ^ dOut1
   -> ( Signal domPin Bit -- packagePin
      , Signal domIn Bit  -- dIn0
      , Signal domIn Bit  -- dIn1
-     )
+     ) -- ^ (packagePin, dIn0, dIn1)
 ioPrim !_ !_ !_ !_ !_ !_ !_ !_ !_ !_ !_ = (pure 0, pure 0, pure 0)
 
 -- | Input pin configuration parameter
