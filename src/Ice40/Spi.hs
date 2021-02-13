68add728a1d8e60fdab6f17f7a808ee5751a8e14
dopamane <dwc1295@gmail.com>
2021-02-12 23:08:14 -0500

    testing record outputs

--- src/Ice40/Spi.hs
+++ src/Ice40/Spi.hs
@@ -7,7 +7,7 @@ Maintainer  : standardsemiconductor@gmail.com
 
 SPI hard IP primitive from Lattice Ice Technology Library https://github.com/standardsemiconductor/VELDT-info/blob/master/SBTICETechnologyLibrary201708.pdf
 -}
-module Ice40.Spi ( spi ) where
+module Ice40.Spi ( spi, Spi(..) ) where
 
 import Clash.Prelude
 import Ice40.Spi.Prim
@@ -15,6 +15,51 @@ import Ice40.Spi.Prim
 --import Data.String.Interpolate (i)
 --import Data.String.Interpolate.Util (unindent)
 
+data Spi = Spi
+  { sbdato  :: BitVector 8 
+  , sbacko  :: Bool
+  , spiirq  :: Bit
+  , spiwkup :: Bit
+  , wo      :: Bit
+  , woe     :: Bit
+  , bo      :: Bit
+  , boe     :: Bit
+  , wcko    :: Bit
+  , wckoe   :: Bit
+  , bcsno   :: BitVector 4
+  , bcsnoe  :: BitVector 4
+  }
+
+mkSpi
+  :: Unbundled dom
+       ( BitVector 8 -- sbdato
+       , Bool        -- sbacko
+       , Bit         -- spiirq
+       , Bit         -- spiwkup
+       , Bit         -- wo
+       , Bit         -- woe
+       , Bit         -- bo
+       , Bit         -- boe
+       , Bit         -- wcko
+       , Bit         -- wckoe
+       , BitVector 4 -- bcsno
+       , BitVector 4 -- bcsnoe
+       )
+  -> Signal dom Spi
+mkSpi (sbdato', sbacko', spiirq', spiwkup', wo', woe', bo', boe', wcko', wckoe', bcsno', bcsnoe') =
+  Spi <$> sbdato'
+      <*> sbacko'
+      <*> spiirq'
+      <*> spiwkup'
+      <*> wo'
+      <*> woe'
+      <*> bo'
+      <*> boe'
+      <*> wcko'
+      <*> wckoe'
+      <*> bcsno'
+      <*> bcsnoe'
+                    
 -- | spi primitive wrapper
 spi
   :: HiddenClock dom
@@ -27,6 +72,8 @@ spi
   -> Signal dom Bit           -- ^ wi
   -> Signal dom Bit           -- ^ wcki
   -> Signal dom Bit           -- ^ wcsni
+  -> Signal dom Spi
+{-
   -> Unbundled dom 
        ( BitVector 8 -- sbdato
        , Bool        -- sbacko
@@ -40,32 +87,33 @@ spi
        , Bit         -- wckoe
        , BitVector 4 -- bcsno
        , BitVector 4 -- bcsnoe
-       ) 
+       )
+-}
 spi busAddr sbrwi sbstbi sbadri sbdati bi wi wcki wcsni =
-  spiPrim busAddr
-          hasClock
-          sbrwi
-          sbstbi
-          (bitAt 7 sbadri)
-          (bitAt 6 sbadri)
-          (bitAt 5 sbadri)
-          (bitAt 4 sbadri)
-          (bitAt 3 sbadri)
-          (bitAt 2 sbadri)
-          (bitAt 1 sbadri)
-          (bitAt 0 sbadri)
-          (bitAt 7 sbdati)
-          (bitAt 6 sbdati)
-          (bitAt 5 sbdati)
-          (bitAt 4 sbdati)
-          (bitAt 3 sbdati)
-          (bitAt 2 sbdati)
-          (bitAt 1 sbdati)
-          (bitAt 0 sbdati)
-          bi
-          wi
-          wcki
-          wcsni
+  mkSpi $ spiPrim busAddr
+                  hasClock
+                  sbrwi
+                  sbstbi
+                  (bitAt 7 sbadri)
+                  (bitAt 6 sbadri)
+                  (bitAt 5 sbadri)
+                  (bitAt 4 sbadri)
+                  (bitAt 3 sbadri)
+                  (bitAt 2 sbadri)
+                  (bitAt 1 sbadri)
+                  (bitAt 0 sbadri)
+                  (bitAt 7 sbdati)
+                  (bitAt 6 sbdati)
+                  (bitAt 5 sbdati)
+                  (bitAt 4 sbdati)
+                  (bitAt 3 sbdati)
+                  (bitAt 2 sbdati)
+                  (bitAt 1 sbdati)
+                  (bitAt 0 sbdati)
+                  bi
+                  wi
+                  wcki
+                  wcsni
 
 bitAt :: KnownNat n => Index n -> Signal dom (BitVector n) -> Signal dom Bit
 bitAt n = fmap (!n)
