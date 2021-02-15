3bc995f7928c50939546e6ce04100d92bd0d336d
dopamane <dwc1295@gmail.com>
2021-02-14 19:46:02 -0500

    drafting pll IP primitive

--- src/Ice40/Pll.hs
+++ src/Ice40/Pll.hs
@@ -58,32 +58,10 @@ import Data.String.Interpolate.Util (unindent)
          , BitVector 4 -- bcsnoe
          )"
         , "template" :
-  "//SB_SPI begin
-  wire ~GENSYM[sbdato7][0];
-  wire ~GENSYM[sbdato6][1];
-  wire ~GENSYM[sbdato5][2];
-  wire ~GENSYM[sbdato4][3];
-  wire ~GENSYM[sbdato3][4];
-  wire ~GENSYM[sbdato2][5];
-  wire ~GENSYM[sbdato1][6];
-  wire ~GENSYM[sbdato0][7];
-  wire ~GENSYM[sbacko][8];
-  wire ~GENSYM[irq][9];
-  wire ~GENSYM[wkup][10];
-  wire ~GENSYM[wo][11];
-  wire ~GENSYM[woe][12];
-  wire ~GENSYM[bo][13];
-  wire ~GENSYM[boe][14];
-  wire ~GENSYM[wcko][15];
-  wire ~GENSYM[wckoe][16];
-  wire ~GENSYM[bcsno3][17];
-  wire ~GENSYM[bcsno2][18];
-  wire ~GENSYM[bcsno1][19];
-  wire ~GENSYM[bcsno0][20];
-  wire ~GENSYM[bcsnoe3][21];
-  wire ~GENSYM[bcsnoe2][22];
-  wire ~GENSYM[bcsnoe1][23];
-  wire ~GENSYM[bcsnoe0][24];
+  "//SB_PLL40_CORE begin
+  wire ~GENSYM[lock][0];
+  wire ~GENSYM[plloutglobal][1];
+  wire ~GENSYM[plloutcore][2];
 
   SB_PLL40_CORE #(
     .FEEDBACK_PATH                  ( ~ARG[0] ),
@@ -94,85 +72,27 @@ import Data.String.Interpolate.Util (unindent)
     .SHIFTREG_DIV_MODE              ( ~ARG[5] ),
     .PLLOUT_SELECT                  ( ~ARG[6] ),
     .DIVR                           ( ~ARG[7] )
-  ) ~GENSYM[sb_spi_inst][25] (
-    .SBCLKI  ( ~ARG[1]  ),
-    .SBRWI   ( ~ARG[2]  ),
-    .SBSTBI  ( ~ARG[3]  ),
-    .SBADRI7 ( ~ARG[4]  ),
-    .SBADRI6 ( ~ARG[5]  ),
-    .SBADRI5 ( ~ARG[6]  ),
-    .SBADRI4 ( ~ARG[7]  ),
-    .SBADRI3 ( ~ARG[8]  ),
-    .SBADRI2 ( ~ARG[9]  ),
-    .SBADRI1 ( ~ARG[10] ),
-    .SBADRI0 ( ~ARG[11] ),
-    .SBDATI7 ( ~ARG[12] ),
-    .SBDATI6 ( ~ARG[13] ),
-    .SBDATI5 ( ~ARG[14] ),
-    .SBDATI4 ( ~ARG[15] ),
-    .SBDATI3 ( ~ARG[16] ),
-    .SBDATI2 ( ~ARG[17] ),
-    .SBDATI1 ( ~ARG[18] ),
-    .SBDATI0 ( ~ARG[19] ),
-    .MI      ( ~ARG[20] ),
-    .SI      ( ~ARG[21] ),
-    .SCKI    ( ~ARG[22] ),
-    .SCSNI   ( ~ARG[23] ),
+  ) ~GENSYM[sb_spi_inst][3] (
+    .REFERENCECLK    ( ~ARG[8]  ),
+    .RESETB          ( ~ARG[9]  ),
+    .BYPASS          ( ~ARG[10] ),
+    .EXTFEEDBACK     ( ~ARG[11] ),
+    .DYNAMICDELAY    ( ~ARG[12] ),
+    .LATCHINPUTVALUE ( ~ARG[13] ),
+    .SCLK            ( ~ARG[14] ),
+    .SDI             ( ~ARG[15] ),
+    .SDO             ( ~ARG[16] ),
 
-    .SBDATO7 ( ~SYM[0]  ),
-    .SBDATO6 ( ~SYM[1]  ),
-    .SBDATO5 ( ~SYM[2]  ),
-    .SBDATO4 ( ~SYM[3]  ),
-    .SBDATO3 ( ~SYM[4]  ),
-    .SBDATO2 ( ~SYM[5]  ),
-    .SBDATO1 ( ~SYM[6] ),
-    .SBDATO0 ( ~SYM[7] ),
-    .SBACKO  ( ~SYM[8] ),
-    .SPIIRQ  ( ~SYM[9] ),
-    .SPIWKUP ( ~SYM[10] ),
-    .SO      ( ~SYM[11] ),
-    .SOE     ( ~SYM[12] ),
-    .MO      ( ~SYM[13] ),
-    .MOE     ( ~SYM[14] ),
-    .SCKO    ( ~SYM[15] ),
-    .SCKOE   ( ~SYM[16] ),
-    .MCSNO3  ( ~SYM[17] ),
-    .MCSNO2  ( ~SYM[18] ),
-    .MCSNO1  ( ~SYM[19] ),
-    .MCSNO0  ( ~SYM[20] ),
-    .MCSNOE3 ( ~SYM[21] ),
-    .MCSNOE2 ( ~SYM[22] ),
-    .MCSNOE1 ( ~SYM[23] ),
-    .MCSNOE0 ( ~SYM[24] )
+    .LOCK         ( ~SYM[0]  ),
+    .PLLOUTGLOBAL ( ~SYM[1]  ),
+    .PLLOUTCORE   ( ~SYM[2]  )
   );
 
-  assign ~RESULT = { ~SYM[0]  // sbdato7
-                   , ~SYM[1]  // sbdato6
-                   , ~SYM[2]  // sbdato5
-                   , ~SYM[3]  // sbdato4
-                   , ~SYM[4]  // sbdato3
-                   , ~SYM[5]  // sbdato2
-                   , ~SYM[6]  // sbdato1
-                   , ~SYM[7]  // sbdato0
-                   , ~SYM[8]  // sbacko
-                   , ~SYM[9]  // spiirq
-                   , ~SYM[10] // spiwkup
-                   , ~SYM[11] // wo
-                   , ~SYM[12] // woe
-                   , ~SYM[13] // bo
-                   , ~SYM[14] // boe
-                   , ~SYM[15] // wcko
-                   , ~SYM[16] // wckoe
-                   , ~SYM[17] // bcsno3
-                   , ~SYM[18] // bcsno2
-                   , ~SYM[19] // bcsno1
-                   , ~SYM[20] // bcsno0
-                   , ~SYM[21] // bcsnoe3
-                   , ~SYM[22] // bcsnoe2
-                   , ~SYM[23] // bcsnoe1
-                   , ~SYM[24] // bcsnoe0
+  assign ~RESULT = { ~SYM[0]  // lock
+                   , ~SYM[1]  // plloutglobal
+                   , ~SYM[2]  // plloutcore
                    };
-  // SB_SPI end"
+  // SB_PLL40_CORE end"
        }
      }
   ]
